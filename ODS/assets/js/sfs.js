21c815e7085157261d8176a2e75c30a769df5dcf
Carolyn <carolynwarburton@hotmail.com>
2015-05-13 14:04:01 +0100

    SFS questions

---
+++ ODS/assets/js/sfs.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function() {
+
+	$("tr input").first().focus();
+
+	$("input").on("change", function(e){
+		e.preventDefault();
+		totalamount = 0;
+
+		$('tr input.amount').each(function(){
+			if ($(this).val()){
+				var amt = $(this).val();
+				totalamount+= tonum(amt);
+			}
+		});
+		if (totalamount && totalamount > 0){
+			$("#box-total").text(totalamount.toFixed(2));
+
+		}
+
+	});
+
+	
+	function tonum(num)
+	{
+	 	return parseFloat(num); //.replace(/[^0-9\.]+/g,"")
+	}
+
+});
