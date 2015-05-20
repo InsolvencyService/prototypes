c5750f9d6d1f5f8e378ef227e7abb928dbe2c493
Carolyn <carolynwarburton@hotmail.com>
2015-05-20 10:56:26 +0100

    Brenda

--- ODS/assets/js/_vti_cnf/jquery.cookie.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|18 Mar 2015 20:56:50 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|SFS3/3-contributions.html SFS3/1-salary-and-wages.html SFS3/4-other-income.html SFS3/costs5.html SFS3/costs6.html SFS3/costs7.html SFS3/costs8.html SFS3/3-pensions.html SFS3/costs4.html SFS3/costs2.html SFS3/costs3.html SFS3/costs11.html SFS3/costs12.html SFS3/costs13.html SFS3/1a.html SFS3/2.html SFS3/3-no-pension.html SFS3/summary.html SFS3/costs10.html SFS3/costs1.html SFS3/FixedCosts/9-notused.html SFS3/costs9.html
-vti_author:SR|CAROLYNSW8\\Carolyn
-vti_modifiedby:SR|CAROLYNSW8\\Carolyn
-vti_timecreated:TR|18 Mar 2015 20:56:50 -0000
-vti_cacheddtm:TX|18 Mar 2015 20:56:50 -0000
-vti_filesize:IR|3169
--- ODS/assets/js/_vti_cnf/jquery.ui-touch-punch.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|11 Mar 2015 07:50:17 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|index.html
-vti_author:SR|CAROLYNSW8\\Carolyn
-vti_modifiedby:SR|CAROLYNSW8\\Carolyn
-vti_timecreated:TR|11 Mar 2015 07:50:17 -0000
-vti_cacheddtm:TX|11 Mar 2015 07:50:17 -0000
-vti_filesize:IR|1304
