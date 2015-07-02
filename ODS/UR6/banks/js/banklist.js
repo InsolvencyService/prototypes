cc99a02b5ef6b5ace2671d144a43253b39573157
Mark Keeling <markkeeling0@gmail.com>
2015-07-02 09:19:26 +0100

    Bank details update

---
+++ ODS/UR6/banks/js/banklist.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function() {
+   
+var creditors =   [
+{sort: '401928', bank: 'HSBC', address: 'High St, Dudley, West Midlands, DY4 9PR'},
+//{sort: '', bank: '', address: ''};
+]
+});
