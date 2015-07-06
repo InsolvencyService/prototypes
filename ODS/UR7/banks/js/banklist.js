c8b5abdebf48b722b7bea184a67efe5170fa996c
Carolyn <carolynwarburton@hotmail.com>
2015-07-06 17:44:27 +0100

    Rolled back and re applied

---
+++ ODS/UR7/banks/js/banklist.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function() {
+   
+var bank =   [
+{sort: '401928', bank: 'HSBC', address: 'High St, Dudley, West Midlands, DY4 9PR'},
+{sort: '000000', bank: 'INSS', address: 'Colmore Row, Birmingham, B1 3RG'}
+
+];
+});
+
+$.each(bank, function( key, value ) {
+	alert( key + ": " + value );
+});	
+
