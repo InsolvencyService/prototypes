1780cdc793a368314c2b4d241ae51d14bddf3afa
Mark Keeling <markkeeling0@gmail.com>
2015-07-14 14:27:49 +0100

    split bank list from page

--- ODS/UR8/banks/js/banklist.js
+++ ODS/UR8/banks/js/banklist.js
@@ -1,13 +1,10 @@
 ﻿$(document).ready(function() {
    
-var bank =   [
-{sort: '401928', bank: 'HSBC', address: 'High St, Dudley, West Midlands, DY4 9PR'},
-{sort: '000000', bank: 'INSS', address: 'Colmore Row, Birmingham, B1 3RG'}
-
+var bank = 	[
+			{sort:'401928', bank:'HSBC', address:'High St, Dudley, West Midlands, DY4 9PR'},
+			{sort: '000000', bank: 'INSS', address: 'Colmore Row, Birmingham, B1 3RG'}
 ];
-});
 
-$.each(bank, function( key, value ) {
-	alert( key + ": " + value );
+
 });	
 
