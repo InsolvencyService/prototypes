8c03d648149184949a08e9ae0e69dbe653e37c7f
Mark Keeling <markkeeling0@gmail.com>
2015-07-14 15:22:45 +0100

    update scripts and details

--- ODS/UR8/banks/js/banklist.js
+++ ODS/UR8/banks/js/banklist.js
@@ -1,20 +1,4 @@
-﻿$(document).ready(function() {
-   
-var bank = 	[
-			{sort:'401928', bank:'HSBC', address:'High St, Dudley, West Midlands, DY4 9PR'},
-			{sort: '000000', bank: 'INSS', address: 'Colmore Row, Birmingham, B1 3RG'}
-];
+﻿
 
-var sortcode = $.cookie("bank_sortcode");
-
-$.map(bank, function(a) {
-	if(a.sort==sortcode){
-		$("#sort_code").val(a.sort);
-		$("#bank").val(a.bank);
-		$("#address").val(a.address);
-	}	
-});
-
-
-});	
+	
 
