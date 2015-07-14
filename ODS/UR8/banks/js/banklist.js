adf0f06210d6c58c96d7e15e461eb9d76314f16c
Mark Keeling <markkeeling0@gmail.com>
2015-07-14 14:37:02 +0100

    updates to bank

--- ODS/UR8/banks/js/banklist.js
+++ ODS/UR8/banks/js/banklist.js
@@ -1,8 +1,8 @@
 ﻿$(document).ready(function() {
    
 var bank = 	[
-			{sort:'401928', bank:'HSBC', address:'High St, Dudley, West Midlands, DY4 9PR'},
-			{sort: '000000', bank: 'INSS', address: 'Colmore Row, Birmingham, B1 3RG'}
+			{sc:'401928', bank:'HSBC', address:'High St, Dudley, West Midlands, DY4 9PR'},
+			{sc: '000000', bank: 'INSS', address: 'Colmore Row, Birmingham, B1 3RG'}
 ];
 
 
