ec7767d2da53146419887eab2555b9b5064f3fd4
Mark Keeling <markkeeling0@gmail.com>
2016-01-06 14:38:02 +0000

    Creation of UR16

---
+++ ODS/UR16/shared/loop.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function() {
+	var loop = getParameterByName("loop");
+
+	if (loop && loop.length > 0){
+
+		$(".repeat").show();
+		$(".firsttime").hide();
+	}
+	else{
+		$(".firsttime").show();
+		$(".repeat").hide();
+	}
+
+});
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
